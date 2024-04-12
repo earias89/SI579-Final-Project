14d82631ba2d8d2b01b26e182002f563d285502b
earias89 <122408755+earias89@users.noreply.github.com>
2024-04-12 17:59:15 -0400

    Delete SI579_FinalProject directory

--- SI579_FinalProject/js/sectionC.js
+++
@@ -1,9 +0,0 @@
-
-function showInfo(id) {
-  document.getElementById("info" + id).style.display = "block";
-}
-
-function hideInfo(id) {
-  document.getElementById("info" + id).style.display = "none";
-}
-
