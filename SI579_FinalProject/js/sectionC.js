08b35ecb1b1abd1f626271e01f3eee725bd3d4fb
earias89 <122408755+earias89@users.noreply.github.com>
2024-04-12 17:37:57 -0400

    Add files via upload

---
+++ SI579_FinalProject/js/sectionC.js
@@ -0,0 +1,9 @@
+
+function showInfo(id) {
+  document.getElementById("info" + id).style.display = "block";
+}
+
+function hideInfo(id) {
+  document.getElementById("info" + id).style.display = "none";
+}
+
